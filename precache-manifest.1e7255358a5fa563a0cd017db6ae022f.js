1d8fe8063b65a493fbcade9b9f2eb69476a881d9
Vincent Man <vincentman2702@hotmail.com>
2019-10-20 21:34:48 +0100

    Updates

---
+++ precache-manifest.1e7255358a5fa563a0cd017db6ae022f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534645b809e7d51cd083e6496135f023",
+    "url": "/worldwide-houses/index.html"
+  },
+  {
+    "revision": "f7f26906ac25ebfdaf49",
+    "url": "/worldwide-houses/static/css/2.423b2911.chunk.css"
+  },
+  {
+    "revision": "cc8848adeb683692d89f",
+    "url": "/worldwide-houses/static/css/main.bb84b9fd.chunk.css"
+  },
+  {
+    "revision": "f7f26906ac25ebfdaf49",
+    "url": "/worldwide-houses/static/js/2.9c9aea0c.chunk.js"
+  },
+  {
+    "revision": "cc8848adeb683692d89f",
+    "url": "/worldwide-houses/static/js/main.85f27eda.chunk.js"
+  },
+  {
+    "revision": "21f235dfcd77507c2aa6",
+    "url": "/worldwide-houses/static/js/runtime-main.a209d08d.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/worldwide-houses/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/worldwide-houses/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/worldwide-houses/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/worldwide-houses/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/worldwide-houses/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/worldwide-houses/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/worldwide-houses/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/worldwide-houses/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/worldwide-houses/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/worldwide-houses/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/worldwide-houses/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/worldwide-houses/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/worldwide-houses/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/worldwide-houses/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/worldwide-houses/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "f0556276fd3a657d6e89857af2dc2886",
+    "url": "/worldwide-houses/static/media/logo.f0556276.png"
+  }
+]);
